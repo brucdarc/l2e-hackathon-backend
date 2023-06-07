c9554c7ab354fbfdc3a9f111a6b9c8421e142fdf
DakBuk <brucdarc@gmail.com>
2023-06-06 21:31:04 -0600

    Add signing and claiming

--- src/masterWallet.ts
+++ src/masterWallet.ts
@@ -6,11 +6,38 @@ const rpc_url = 'WALLET_SECRET_KEY'
 const WALLET_SECRET_KEY = '2e0232d624a8935256dae02ce72e7551fa05fd84c385285d641c19e669e08c5f'
 
 
-const provider = new ethers. //providers.JsonRpcProvider(URL);
 const wallet = new ethers.Wallet(WALLET_SECRET_KEY);
 // connect the wallet to the provider
-const signer = wallet.connect(provider);
+export const signClaim = async (address: string, amount: string, nonce: string) => {
 
+  const hashedMessage = ethers.solidityPackedKeccak256(['address', 'uint256', 'uint256'], [address, amount, nonce]);
 
+  const digest = ethers.hashMessage(ethers.getBytes(hashedMessage));
+
+  console.log('hash message ', hashedMessage);
+
+  console.log('digest ', digest);
+
+  const signature = await wallet.signMessage(ethers.getBytes(hashedMessage));
+
+  console.log('sig hash ', signature);
+
+  const sig  = ethers.Signature.from(signature);
+
+  console.log('sig ', sig);
+
+  console.log('v ', sig.v)
+  console.log('r ', sig.r)
+  console.log('s ', sig.s)
+
+  console.log('wallet address ', wallet.address);
+
+  const rec = ethers.recoverAddress(digest, signature);
+
+  console.log('recovered address ', rec);
+
+  return sig;
+
+}
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,9 @@ import { Router, Request, Response } from 'express';
 import database from "./database";
 import {createClient} from "redis";
 
-import { ethers } from 'ethers';
+import {signClaim} from './masterWallet';
+
+import {ethers, toNumber} from 'ethers';
 
 const connectClient = async () => {
   const client = createClient();
@@ -19,22 +21,6 @@ const routes = Router();
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
-
-// define a route handler for the default home page
-routes.get( "/old", (req: Request, res: Response) => {
-  res.send("Hello world!");
-});
-
-routes.get('/balls', (req: Request, res: Response) => {
-  //console.log(req)
-  res.send("Beanus Penus");
-});
-
-routes.get('/beans', (req: Request, res: Response) => {
-  //console.log(req)
-  res.send("Beanus Penuses");
-});
-
 routes.post( '/upload', async (req: Request, res: Response) => {
 
   const client = createClient();
@@ -93,23 +79,60 @@ routes.post('/pause', async (req: Request, res: Response) => {
   console.log('pause ', address)
 })
 
+routes.post('/claimable', async (req, res) => {
+  const client = await connectClient();
+  const address = req.body.address
+
+  await client.hSet(address,
+    "playing", "false"
+  )
+
+  const playStartTime = await client.hGet(address, "timestamp")
+
+  const TotalPlayTime = Date.now() - Number(playStartTime);
+
+  //Time values are stored in unix timestamp format, but to the millisecond level
+  //So they have 3 extra decimal places compared to regular unix timestamps
+  await client.hIncrBy(address,
+    "claimable_time", TotalPlayTime
+  )
+
+  const accumulatedTime = await client.hGet(address, "claimable_time")
+
+  const claimable_tokens = toNumber(accumulatedTime) / 3600000;
+
+  res.json({
+    claimable_tokens: claimable_tokens,
+  });
+});
+
 routes.post('/claim', async (req: Request, res: Response) => {
   const client = await connectClient();
   const address = req.body.address
 
   const accumulatedTime = await client.hGet(address, "claimable_time")
 
+  const nonce = await client.get('nonce');
+
   await client.hSet(address,
     "claimable_time", 0
   )
 
-  console.log("User ", address, " claimed ", accumulatedTime, " seconds of play time")
+  await client.incrBy(
+    "nonce", 1
+  );
 
-  //@TODO Sign message containing claimable time and users address to be sent by user on chain to claim
+  console.log("User ", address, " claimed ", accumulatedTime, " seconds of play time with nonce ", nonce);
+
+  const sig = await signClaim(address, accumulatedTime, nonce);
 
   res.json({
-    message: 'This message would contain a signature from the server if that were implemented',
-    claimed_time: accumulatedTime
+    message: 'This message contains a signature from the server',
+    claimed_time: accumulatedTime,
+    nonce: nonce,
+    v: sig.v,
+    r: sig.r,
+    s: sig.s,
   })
 })
 
